5bbcf9a5a6ae9929c243874c5336e4965a6cf3d2
bburton5 <bburton5@uw.edu>
2022-10-22 11:50:00 -0700

    GET routes for Category are functional

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,43 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const allCategories = await Category.findAll({
+    include: [Product],
+  })
+    .then((allCategories) => res.json(allCategories))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const oneCategory = await Category.findByPk(req.params.id, {
+    include: [Product],
+  })
+    .then((oneCategory) => res.json(oneCategory))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
